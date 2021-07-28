3c185e645ad6b4ab867bdb667fba1a6a5ab1053d
NickoBrown <88066618+NickoBrown@users.noreply.github.com>
2021-07-28 09:54:33 +0800

    First commit for activity 1

---
+++ Activity 1/DoubleLinkedList/src/doublelinkedlist/Country.java
@@ -0,0 +1,46 @@
+package doublelinkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Country {
+
+    String name;
+    List<City> cities = new ArrayList<City>();
+
+    /**
+     * Country class.
+     *
+     * @param name name of the city.
+     * @param cities list of city names, creates a city object for each string
+     * in the list.
+     */
+    public Country(String name, String[] cities) {
+
+        this.name = name;
+        for (String cityName : cities) {
+            City city = new City();
+            city.name = cityName;
+            city.country = this;
+            this.cities.add(city);
+
+        }
+    }
+
+    /**
+     * Nested city class representing a city in a country.
+     */
+    private class City {
+
+        String name;
+        Country country;
+        
+        @Override
+        public String toString() {
+        return name;
+    }
+    }
+
+    
+
+}
